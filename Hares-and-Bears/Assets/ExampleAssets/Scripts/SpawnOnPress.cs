6e679c45da88237e4092757635b086fef53df48d
Robin B <rbertin@kth.se>
2021-11-23 15:23:37 +0100

    UI with 3D hare & fox

--- Hares-and-Bears/Assets/ExampleAssets/Scripts/SpawnOnPress.cs
+++ Hares-and-Bears/Assets/ExampleAssets/Scripts/SpawnOnPress.cs
@@ -5,32 +5,26 @@ using UnityEngine.UI;
 
 public class SpawnOnPress : MonoBehaviour
 {
-    public GameObject Rabbit;
-    public GameObject Bear;
-    public GameObject Fox;
+    [SerializeField] private GameObject animalPrefab;
 
-    public Button yourButton;
-    public Dropdown choices;
-    
-    private bool pressed = false;
+    private bool activated = false;
 
-    public Canvas canvas;
+    public Canvas generalCanvas;
 
     private float cooldown = 0.5f;
     private float lastTime = 0.0f;
     
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        gameObject.GetComponent<Button>().onClick.AddListener(spawning); //Connect spawning function with button
-    }
 
     public void OnPlacementEnd()
     {
-        pressed = true;
-        canvas.gameObject.SetActive(true);
+        activated = true;
+        generalCanvas.gameObject.SetActive(true);
+    }
 
+    public void OnChangePrefab(GameObject newPrefab)
+    {
+        animalPrefab = newPrefab;
     }
 
     
@@ -46,42 +40,14 @@ public class SpawnOnPress : MonoBehaviour
         RaycastHit hit;
         
         //only if ray hits plane and button was pressed
-        if (Physics.Raycast(ray, out hit) && pressed)
+        if (Physics.Raycast(ray, out hit) && activated)
         {
             lastTime = Time.time;
-        
-            //Debug.DrawLine(ray.origin, hit.point);
-            
-            
-            //check animal condition
-            //Spawn animal depending on what choice was set
-            if (choices.captionText.text == "Rabbit")
-            {
-                Instantiate(Rabbit, hit.point, Quaternion.identity);
-            }
 
-            if (choices.captionText.text == "Fox")
-            {
-                Instantiate(Fox, hit.point, Quaternion.identity);
-            }
+            Instantiate(animalPrefab, hit.point, Quaternion.identity);
 
-            if (choices.captionText.text == "Bear")
-            {
-                Instantiate(Bear, hit.point, Quaternion.identity);
-            }
         }
         
-        else {
-            //change button state back to inactive
-        }
     }
-        
-        private void spawning()
-        {
-            Debug.Log("You have clicked the button!");
-            Debug.Log(choices.captionText.text); //Get string of active choice from dropdown object
-            pressed ^= false;
-        
-        }
 
 }
