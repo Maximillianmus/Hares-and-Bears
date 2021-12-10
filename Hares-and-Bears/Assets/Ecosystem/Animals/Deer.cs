5688560e219162f9bd89bf4fc3adc6de2175888d
Malin Liedholm <malin.liedholm@gmail.com>
2021-12-10 16:36:17 +0100

    started working on animator for deer

--- Hares-and-Bears/Assets/Ecosystem/Animals/Deer.cs
+++ Hares-and-Bears/Assets/Ecosystem/Animals/Deer.cs
@@ -6,23 +6,33 @@ public class Deer : Animal
 {
     // Deer male mesh
     public Mesh maleModelMesh;
+    public Animator animator;
 
     new void Start()
     {
         // Call start method from base class
         base.Start();
-
         checkGender();
+    }
+
+    new void Update()
+    {
+        base.Update();
+
     }
 
     // Check if male and change mesh to male mesh in that case
     private void checkGender()
     {
+        Transform deerPrefab = prefab.transform.Find("Deer");
+        if(deerPrefab != null) {
+            animator = deerPrefab.gameObject.GetComponent<Animator>();
+        }
+
         if (male)
         {
-            //print("is male");
+            print("is male");
 
-            Transform deerPrefab = prefab.transform.Find("Deer");
             if (deerPrefab != null)
             {
                 Transform deerModel = deerPrefab.gameObject.transform.Find("DeerModel");
