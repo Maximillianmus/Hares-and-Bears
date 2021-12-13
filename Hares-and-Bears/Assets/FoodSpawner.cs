4468dbb1cc89483c3f46f99c9d2ec29ef126cd2f
Robin B <rbertin@kth.se>
2021-12-13 10:05:58 +0100

    Having a hand as a model

--- Hares-and-Bears/Assets/FoodSpawner.cs
+++ Hares-and-Bears/Assets/FoodSpawner.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class FoodSpawner : MonoBehaviour
@@ -9,12 +10,14 @@ public class FoodSpawner : MonoBehaviour
     [SerializeField] private Camera arCam;
     private GameObject food = null;
     
+    
     public void OnSpawnFood()
     {
         if (food != null) return;
-        var positionHand = ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.palm_center;
+        var hand = ManomotionManager.Instance.Hand_infos[0].hand_info;
+        var positionHand = hand.tracking_info.palm_center;
         var spawnPos = ManoUtils.Instance.CalculateNewPosition(positionHand,
-            ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.depth_estimation);
+            hand.tracking_info.depth_estimation);
         food = Instantiate(foodPrefab, spawnPos, Quaternion.identity);
     }
 
@@ -29,10 +32,16 @@ public class FoodSpawner : MonoBehaviour
     public void Update()
     {
         if (food == null) return;
-        var positionHand = ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.palm_center;
+
+        var hand = ManomotionManager.Instance.Hand_infos[0].hand_info;
+        var positionHand = hand.tracking_info.palm_center;
         var newPos = ManoUtils.Instance.CalculateNewPosition(positionHand,
-            ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info.depth_estimation);
+            hand.tracking_info.depth_estimation);
         food.transform.position = newPos;
+        var bodyPosHand = arCam.transform.position - new Vector3(0, (arCam.transform.position.y - newPos.y), 0);
+        var bodyToHand = bodyPosHand - newPos;
+        food.transform.rotation = Quaternion.LookRotation(bodyToHand);
+        food.transform.Rotate(0, 180, 0);
 
     }
 }
