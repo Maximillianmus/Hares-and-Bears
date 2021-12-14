e70734a08300dfae467f8b3f2c417fb24476bd2b
Robin B <rbertin@kth.se>
2021-12-14 11:22:31 +0100

    Add number of plants that should be spawned at generation

--- Hares-and-Bears/Assets/Map Generation/SpawnAnimalsPlants.cs
+++ Hares-and-Bears/Assets/Map Generation/SpawnAnimalsPlants.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Assertions;
 
 public class SpawnAnimalsPlants : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class SpawnAnimalsPlants : MonoBehaviour
     public GameObject rabbitPrefab;
     public GameObject foxPrefab;
     public List<GameObject> plantPrefabs = new List<GameObject>();
+    public List<int> plantSpawnNumber = new List<int>();
 
     public GameObject treePrefab;
     public GameObject deerPrefab;
@@ -46,11 +48,22 @@ public class SpawnAnimalsPlants : MonoBehaviour
         Spawn(foxPrefab);
         Spawn(bearPrefab);
 
-        for (int i = 0; i < numberOfPlantsBegin; ++i)
+        if (plantSpawnNumber.Count != plantPrefabs.Count)
         {
-            var plant = plantPrefabs[Random.Range(0, plantPrefabs.Count)];
-            Spawn(plant);
+            Debug.LogError("A spawn number must be given for each plant !");
+            return;
         }
+
+        for (int i = 0; i < plantPrefabs.Count; ++i)
+        {
+            var toSpawn = plantPrefabs[i];
+            var numbersToSpawn = plantSpawnNumber[i];
+            for (int j = 0; j < numbersToSpawn; ++j)
+            {
+                Spawn(toSpawn);
+            }
+        }
+
 
         int numTrees = Random.Range(minNumberOfTrees, maxNumberOfTrees);
         for(int i = 0; i < numTrees; i++)
